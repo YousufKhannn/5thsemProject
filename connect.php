d05d5e0782d06f0abd57bb4c0a8d9f0791cd6b74
Yousufkhan J Pathan <132833891+YousufKhannn@users.noreply.github.com>
2024-02-23 21:31:58 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,23 @@
+//<?php
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+// $subject = $_POST['subject'];
+// $message = $_POST['message'];
+
+// Database connection 
+
+
+
+//$conn = new mysqli('localhost', 'root', '' , 'aeipdb');
+//if($conn->connect_error){
+  //   die('connection Failed : ' .$conn->connect_error);
+// }else{
+  //   $stmt = $conn->prepare("insert into emailregistration(name , email , subject , message")
+  //   values("?,?,?,?")
+  //   $stmt->bind_param("ssss", $name , $email , $subject , $message);
+  //   $stmt->execute();
+  //   echo " Registration Successfully";
+  //   $stmt->close();
+  //   $conn->close();
+// }
+// ?>
